040c5955ff30c0b839820e620110a1cdfdd72edf
Gustavo Alves <dev.alvesdossan@gmail.com>
2025-11-24 17:04:37 -0300

    resumo: Questão de Treino com JS sobre as Variaveis

---
+++ Resumo Variaveis/Questões Elaboradas pelo GPT/index.js
@@ -0,0 +1,29 @@
+/*1. Criar um arquivo JS e testar variáveis
+
+Crie um arquivo chamado variaveis.js e faça:
+
+Declare 3 variáveis usando let
+
+Declare 3 constantes usando const
+
+Mostre os valores no console com console.log() */
+
+
+let variavelUm = "Olá, Mundo!";
+
+let variavelDois = 45;
+
+let variavelTres = "Gustavo Alves";
+
+const Resolucao = "Não Muda";
+
+const AnoAtual = 2025;
+
+const dataAniversario = "02/05/2006";
+
+console.log(variavelUm);
+console.log(variavelTres);
+console.log(variavelDois);
+
+console.log(Resolucao, AnoAtual, dataAniversario);
+
